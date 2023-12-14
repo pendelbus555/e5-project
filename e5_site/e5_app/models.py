8983849d753297f7f40a1d1b85b28e60f29f9f4f
pendelbus <mrusipusi@mail.ru>
2023-12-14 19:04:56 +0300

    Adding postgres and creating model News

--- e5_site/e5_app/models.py
+++ e5_site/e5_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class News(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.TextField(max_length=500)
+    created_at = models.DateField(auto_now_add=True)
--- e5_site/e5_site/settings.py
+++ e5_site/e5_site/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'e5_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'e5_db',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -106,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
