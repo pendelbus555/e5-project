fc42cb0976d856b5b93d3d51ca437ed9c76a253d
pendelbus <mrusipusi@mail.ru>
2023-12-15 12:03:31 +0300

    Modify models and migrate them

--- e5_site/e5_app/models.py
+++ e5_site/e5_app/models.py
@@ -3,6 +3,18 @@ from django.db import models
 
 # Create your models here.
 class News(models.Model):
-    name = models.CharField(max_length=40)
-    description = models.TextField(max_length=500)
-    created_at = models.DateField(auto_now_add=True)
+    name = models.CharField(max_length=40, blank=True, )
+    picture = models.ImageField(null=True, blank=True)
+    description = models.TextField(max_length=500, )
+    created_at = models.DateTimeField(auto_now_add=True, )
+    updated_at = models.DateTimeField(auto_now=True, )
+
+    def __repr__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'News'
+        verbose_name_plural = 'News'
