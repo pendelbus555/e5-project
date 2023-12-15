708b9c6a38ff125561dacd78cf1d0a2828691d3b
pendelbus <mrusipusi@mail.ru>
2023-12-15 11:07:02 +0300

    Add intial models, admin, views, info in db

--- e5_site/e5_app/models.py
+++ e5_site/e5_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class News(models.Model):
     name = models.CharField(max_length=40)
--- e5_site/e5_app/views.py
+++ e5_site/e5_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News
 
 
 # Create your views here.
 
 def index(request):
+    result = News.objects.all()
     return HttpResponse('hello world')
