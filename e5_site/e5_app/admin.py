4be6d239d51bac9e0ce668c1914e377b2b9956b5
pendelbus <mrusipusi@mail.ru>
2023-12-19 15:14:27 +0300

    Add pillow and some changes in admin panel

--- e5_site/e5_app/admin.py
+++ e5_site/e5_app/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import News
+
+
 # Register your models here.
 
-admin.site.register(News)
+@admin.register(News)
+class AdminSite(admin.ModelAdmin):
+    list_display = ['name', 'get_description', 'created_at', 'updated_at', ]
+    search_fields = ['name',]
+
+    def get_description(self, obj):
+        if len(obj.description) > 30:
+            return obj.description[:30] + '...'
+        return obj.description
