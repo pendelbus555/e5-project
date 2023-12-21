b17b86ee0905db135849a0aff218ff0bda4b6cc7
pendelbus <mrusipusi@mail.ru>
2023-12-21 15:44:06 +0300

    Added admin-site header

--- e5_site/e5_app/admin.py
+++ e5_site/e5_app/admin.py
@@ -4,15 +4,17 @@ from rangefilter.filters import DateRangeQuickSelectListFilterBuilder
 
 # Register your models here.
 
+admin.site.site_header = 'Administration e5'
+
+
 @admin.register(News)
 class AdminSite(admin.ModelAdmin):
     list_display = ['name', 'get_description', 'created_at', 'updated_at', ]
-    search_fields = ['name',]
+    search_fields = ['name', ]
     date_hierarchy = 'created_at'
     list_filter = [
         ('created_at', DateRangeQuickSelectListFilterBuilder()),
     ]
-
 
     def get_description(self, obj):
         if len(obj.description) > 30:
