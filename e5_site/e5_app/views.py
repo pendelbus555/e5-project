e838734589ce6ec66cc6aa8b2f03370a9a4a0a89
mrusipusi <mrusipusi@mail.ru>
2024-01-08 19:31:54 +0300

    Add bootstrap

--- e5_site/e5_app/views.py
+++ e5_site/e5_app/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import News
 
 
 # Create your views here.
 
 def index(request):
-    result = News.objects.all()
-    return HttpResponse('hello world')
+
+    return render(request,'e5_app/index.html')
