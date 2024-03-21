0a1cda26414471e474d63e26d7bc69e7cd86bacf
mrusipusi <mrusipusi@mail.ru>
2024-03-21 10:36:38 +0300

    Add all test by coverage for models

--- e5_site/e5_app/test_models.py
+++ e5_site/e5_app/test_models.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from .models import *
 from django.utils.text import slugify
 from django.utils import timezone
+from django.core import mail
 
 
 class CommonTest(TestCase):
@@ -16,7 +17,8 @@ class NewsTest(TestCase):
     def test_absolute_url(self):
         news_name = 'Test news name'
         news_slug_url = 'test-news-name'
-        news = News.objects.create(name=news_name, created_at=timezone.now(), slug_url=news_slug_url, )
+        news_created_at = timezone.now()
+        news = News.objects.create(name=news_name, created_at=news_created_at, slug_url=news_slug_url, )
         url = news.get_absolute_url()
         self.assertTrue(url, 'News do not have absolute url')
         self.assertEqual(url, f'/site/news/{slugify(news_name)}', 'News have wrong absolute url')
@@ -78,3 +80,73 @@ class HTMLPageTest(TestCase):
         html_page = HTMLPage.objects.create(url_name=html_page_url_name)
         self.assertEqual(repr(html_page), html_page_url_name, 'HTMLPage __repr__ method do not return name')
         self.assertEqual(str(html_page), html_page_url_name, 'HTMLPage __str__ method do not return name')
+
+    def test_update_or_create_page(self):
+        html_page_url_name = 'test'
+        html_page_content_old = 'Test html_page content old'
+        html_page_content_new = 'Test html_page content new'
+        HTMLPage.update_or_create_page(url_name=html_page_url_name, new_content=html_page_content_old)
+        html_page = HTMLPage.objects.get(url_name=html_page_url_name)
+        self.assertEqual(html_page.content, html_page_content_old, 'Content was not delivered')
+        HTMLPage.update_or_create_page(url_name=html_page_url_name, new_content=html_page_content_new)
+        html_page = HTMLPage.objects.get(url_name=html_page_url_name)
+        self.assertEqual(html_page.content, html_page_content_new, 'Content was not changed')
+
+
+class EventTest(TestCase):
+    def test_save(self):
+        mailing_mail = 'test@test.test'
+        event_type = EventType.objects.create()
+        Mailing.objects.create(mail=mailing_mail)
+        event_date = timezone.now()
+        Event.objects.create(date=event_date, event_type=event_type)
+        self.assertEqual(len(mail.outbox), 1, 'Message was not delivered')
+        self.assertEqual(mail.outbox[0].subject, f'Новое мероприятие Э5 {event_date}',
+                         'Message subject is not correct')
+
+
+class EventScheduleTest(TestCase):
+    def test_repr_str(self):
+        event_type = EventType.objects.create()
+        event_date = timezone.now()
+        event_schedule_start = timezone.now().time()
+        event_schedule_end = (timezone.now() + timezone.timedelta(hours=1)).time()
+        event_schedule_place = 'Test event_schedule place'
+        event = Event.objects.create(date=event_date, event_type=event_type)
+        event_schedule = EventSchedule.objects.create(event=event,
+                                                      start=event_schedule_start,
+                                                      end=event_schedule_end,
+                                                      place=event_schedule_place, )
+        self.assertEqual(
+            repr(event_schedule),
+            str(event_schedule_start.strftime("%H:%M")) + '-' + str(
+                event_schedule_end.strftime("%H:%M")) + ' ' + event_schedule_place,
+            'EventSchedule __repr__ method do not return name'
+        )
+        self.assertEqual(
+            str(event_schedule),
+            str(event_schedule_start.strftime("%H:%M")) + '-' + str(
+                event_schedule_end.strftime("%H:%M")) + ' ' + event_schedule_place,
+            'EventSchedule __str__ method do not return name'
+        )
+
+
+class VisitorTest(TestCase):
+    def test_save(self):
+        event_type = EventType.objects.create()
+        event_name = 'Test event name'
+        event_date = timezone.now()
+        event_schedule_start = timezone.now().time()
+        visitor_mail = 'test@test.test'
+        event_schedule_end = (timezone.now() + timezone.timedelta(hours=1)).time()
+        event = Event.objects.create(name=event_name, event_type=event_type, date=event_date)
+        event_schedule = EventSchedule.objects.create(event=event,
+                                                      start=event_schedule_start,
+                                                      end=event_schedule_end, )
+        Visitor.objects.create(event=event_schedule, mail=visitor_mail)
+        self.assertEqual(len(mail.outbox), 1, 'Message was not delivered')
+        self.assertEqual(
+            mail.outbox[0].subject,
+            f'Запись на мероприятие Э5 {event_name}, {event_date}',
+            'Message subject is not correct'
+        )
