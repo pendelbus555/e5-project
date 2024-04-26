4c9c6bfbafc9785bd21943e9f507f4d50e4f4d13
mrusipusi <mrusipusi@mail.ru>
2024-04-26 11:55:17 +0300

    Optimization, add tags for directions and histroy templates

--- e5_app/templatetags/e5_app_extras.py
+++ e5_app/templatetags/e5_app_extras.py
@@ -7,3 +7,21 @@ register = template.Library()
 def show_list(paragraph):
     lst = paragraph.split('\n')
     return {'lst': lst}
+
+
+@register.inclusion_tag('directions_tag.html')
+def directions_tag(start, end, alt, src):
+    counts = map(str, list(range(start, end)))
+    return {'counts': counts, 'alt': alt, 'src': src}
+
+
+@register.inclusion_tag('history_indicator_tag.html')
+def history_indicator_tag(start, end, target):
+    counts = range(start, end)
+    return {'counts': counts, 'target': target}
+
+
+@register.inclusion_tag('history_inner_tag.html')
+def history_inner_tag(start, end, alt, src):
+    counts = map(str, list(range(start, end)))
+    return {'counts': counts, 'alt': alt, 'src': src}
